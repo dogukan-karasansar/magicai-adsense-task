c5ac8c46d243a35e944b1e1d11d895af982ecfa2
Doğukan karasansar <karasansard@gmail.com>
2024-12-14 16:03:30 +0300

    Enhance Adsense index view with improved button styles and icons for copy, edit, and delete actions

--- resources/views/panel/admin/frontend/adsense/index.blade.php
+++ resources/views/panel/admin/frontend/adsense/index.blade.php
@@ -33,7 +33,8 @@
                     New</a>
 
                 @else
-                <a class="btn btn-primary" href="{{route('dashboard.admin.frontend.adsense.createOrUpdate')}}">Add New</a>
+                <a class="btn btn-primary" href="{{route('dashboard.admin.frontend.adsense.createOrUpdate')}}">Add
+                    New</a>
 
                 @endif
             </div>
@@ -76,12 +77,41 @@
                                 <td>
                                     {{-- copy code --}}
                                     <a onclick="copyToClipboard(`{{$ad->generateAdSenseCode()}}`)"
-                                        class="btn btn-primary">{{__('Copy')}}</a>
+                                        class="btn w-[36px] h-[36px] p-0 border hover:bg-[var(--tblr-secondary)] hover:text-white"
+                                        style="cursor: pointer;">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 96 960 960"
+                                            fill="var(--lqd-heading-color)" width="20">
+                                            <path
+                                                d="M180 975q-24 0-42-18t-18-42V312h60v603h474v60H180Zm120-120q-24 0-42-18t-18-42V235q0-24 18-42t42-18h440q24 0 42 18t18 42v560q0 24-18 42t-42 18H300Zm0-60h440V235H300v560Zm0 0V235v560Z">
+                                            </path>
+                                        </svg>
+                                        <span class="sr-only">{{__('Copy Code')}}</span>
+                                    </a>
+
                                     {{-- edit and delete --}}
                                     <a href="{{route('dashboard.admin.frontend.adsense.createOrUpdate', $ad->id)}}"
-                                        class="btn btn-primary">{{__('Edit')}}</a>
-                                    <a href="{{route('dashboard.admin.frontend.adsense.delete', $ad->id)}}"
-                                        class="btn btn-danger">{{__('Delete')}}</a>
+                                        class="btn w-[36px] h-[36px] p-0 border hover:bg-[var(--tblr-primary)] hover:text-white"
+                                        title="Edit">
+                                        <svg width="13" height="12" viewBox="0 0 15 14" fill="none"
+                                            stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M8.71875 2.43988L11.9688 5.58995M10.75 11.4963H14M4.25 13.0714L12.7812 4.80248C12.9946 4.59564 13.1639 4.35009 13.2794 4.07984C13.3949 3.8096 13.4543 3.51995 13.4543 3.22744C13.4543 2.93493 13.3949 2.64528 13.2794 2.37504C13.1639 2.10479 12.9946 1.85924 12.7812 1.6524C12.5679 1.44557 12.3145 1.28149 12.0357 1.16955C11.7569 1.05761 11.458 1 11.1562 1C10.8545 1 10.5556 1.05761 10.2768 1.16955C9.99799 1.28149 9.74465 1.44557 9.53125 1.6524L1 9.92135V13.0714H4.25Z"
+                                                stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round">
+                                            </path>
+                                        </svg>
+                                    </a>
+
+                                    <a onclick="return confirm('Are you sure?');"
+                                        href="{{route('dashboard.admin.frontend.adsense.delete', $ad->id)}}"
+                                        class="btn w-[36px] h-[36px] p-0 border hover:bg-red-500 hover:text-white"
+                                        title="Delete">
+                                        <svg width="10" height="10" viewBox="0 0 10 10" fill="currentColor"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M9.08789 1.74609L5.80664 5L9.08789 8.25391L8.26758 9.07422L4.98633 5.82031L1.73242 9.07422L0.912109 8.25391L4.16602 5L0.912109 1.74609L1.73242 0.925781L4.98633 4.17969L8.26758 0.925781L9.08789 1.74609Z">
+                                            </path>
+                                        </svg>
+                                    </a>
                                 </td>
                             </tr>
                             @endforeach
